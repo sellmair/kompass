a1f41cd658ebe61e5f1324ec0c258a696e29edf0
Sebastian Sellmair <sebastiansellmair@gmail.com>
2019-04-14 02:45:11 +0200

    Polish

--- android-example--fragment/src/main/java/io/sellmair/kompass/android/example/DummyService.kt
+++ android-example--fragment/src/main/java/io/sellmair/kompass/android/example/DummyService.kt
@@ -6,6 +6,66 @@ package io.sellmair.kompass.android.example
 class DummyService {
     companion object {
         var isLoggedIn: Boolean = false
+
+
+        val contacts = listOf(
+            Contact(
+                name = "Julian B.",
+                phone = "-",
+                email = "julian@tikkr.app",
+                nickname = "Julian"
+            ),
+            Contact(
+                name = "Stefan K.",
+                phone = "0176/43404485",
+                email = "stefan@fasteaglefactory.com",
+                nickname = "Steffen"
+            ),
+            Contact(
+                name = "Malte B.",
+                phone = "08442/90909090",
+                email = "malte@fasteaglefactory.com",
+                nickname = "Malthe"
+            ),
+            Contact(
+                name = "Klaus N.",
+                phone = "0800/32-16-8",
+                email = "klaus@rustserver.io",
+                nickname = "Klaus"
+            ),
+            Contact(
+                name = "G. Nasir",
+                phone = "00491500-not-his-own",
+                email = "nasir@slackisthereforareason.com",
+                nickname = "Nasir"
+            ),
+            Contact(
+                name = "Mathias Q.",
+                phone = "0176/46779438",
+                email = "mathias@pleasedontleave.io",
+                nickname = "Mathias"
+            ),
+            Contact(
+                name = "Balazs T.",
+                phone = "0152/837782458",
+                email = "balazs@",
+                nickname = "Balazs"
+            ),
+            Contact(
+                name = "Niko T.",
+                phone = "0800/0800",
+                email = "niko@",
+                nickname = "Niko"
+            ),
+            Contact(
+                name = "Paul K.",
+                phone = "0800/dj-paul-power",
+                email = "super@paul.io",
+                nickname = "Paul"
+            )
+        )
+
+        /*
         val contacts: List<Contact> = mutableListOf<Contact>()
             .also { list ->
                 list.add(
@@ -46,12 +106,12 @@ class DummyService {
 
                 list.add(
                     Contact(
-                        "Joan D.",
+                        "Klaus N",
                         "0176/43505560",
-                        "joan@...", "" +
-                                "Joan"
+                        "klaus@...",
+                        "Klaus"
                     )
-                )
-            }
+                )*
+            }*/
     }
 }
