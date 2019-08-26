ab4591978d48299cb918162bdab34dfb9192354f
Julian Bissekkou <julianbissekkou@quickbirdstudios.com>
2019-08-26 10:48:14 +0200

    decrease minSdkVersion tp 19

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -3,7 +3,7 @@ object Project {
     object Android {
         const val compileSdkVersion = 28
         const val targetSdkVersion = 28
-        const val minSdkVersion = 21
+        const val minSdkVersion = 19
         const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
